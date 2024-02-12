9c0b646ac8db2966c18b3282866030a688a2d03b
Jake <jake@xyzdigital.com>
2024-02-12 13:29:02 -0800

    structure for a Cloudflare based CMS

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,60 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+//@ts-expect-error - i don't think the ts-config is setup to look for html files
+import homePage from './pages/home.html';
+//@ts-expect-error - i don't think the ts-config is setup to look for js files
+import scriptJs from './pages/script.html';
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
+// a function to bundle the dynamic posts into a single object key store
+const pages = {
+	blog: {
+		'hello-world': 'Hello, world!',
+		'another-post': 'This',
+	},
+};
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		const path = url.pathname;
+		const origin = url.origin;
+
+		if (path.startsWith('/blog/')) {
+			const slug = path.split('/').pop();
+			if (slug) {
+				//@ts-ignore - i know what i'm doing
+				return new Response(pages.blog[slug], {
+					headers: { 'Content-Type': 'text/html; charset=utf-8' },
+				});
+			}
+		}
+		switch (path) {
+			case '/':
+				return new Response(homePage, {
+					headers: { 'Content-Type': 'text/html; charset=utf-8' },
+				});
+			case '/script.js':
+				return new Response(scriptJs, {
+					headers: { 'Content-Type': 'text/javascript; charset=utf-8' },
+				});
+			case '/sitemap.xml':
+				const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+				<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+				  <url>
+					<loc>${origin}</loc>
+					<lastmod>${new Date().toISOString()}</lastmod>
+					<changefreq>monthly</changefreq>
+					<priority>1.0</priority>
+				  </url>
+				</urlset>`;
+
+				return new Response(sitemap, {
+					headers: { 'Content-Type': 'application/xml' },
+				});
+			default:
+				return new Response('Page not found', {
+					status: 404,
+					statusText: 'Not Found',
+					headers: { 'Content-Type': 'text/plain; charset=utf-8' },
+				});
+		}
 	},
 };
