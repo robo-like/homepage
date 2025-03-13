8c74a6382525c614ca53b9bf3a5b7def282a2d45
Jacob Berg <jake@dubsado.com>
2025-03-13 14:05:52 -0700

    adds correct functionality for localhost admin check

--- app/routes/admin/layout.tsx
+++ app/routes/admin/layout.tsx
@@ -1,16 +1,11 @@
 import { Outlet } from "react-router";
 import { Header } from "~/components/Header";
-
-export async function loader() {
-    if (typeof window === 'undefined') {
-        return null;
-    }
-
-    if (!window.location.hostname.includes('localhost')) {
+import type { Route } from "./+types/layout"
+export async function loader({ request }: Route.ClientLoaderArgs) {
+    const url = new URL(request.url);
+    if (!url.hostname.includes('localhost')) {
         throw new Response('Not Found', { status: 404 });
     }
-
-    return null;
 }
 
 export default function Layout() {
